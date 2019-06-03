08ccf00d79969e8a97bd44df397ab2540625798d
David Omstead <davidomstead@hotmail.com>
2019-06-03 12:59:30 -0700

    Java edits

--- File.js
+++ File.js
@@ -1,1 +1,6 @@
-console.log("Hi, my name is David and I am 29 years old")
+function createGreeting(){
+    return "Hi, my name is David and I am 29 years old"
+}
+
+const greeting1 = createGreeting();
+console.log(greeting1);
